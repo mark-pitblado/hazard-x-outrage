73cec086cf06ec3228368603aae3b543db8636fa
Mark Pitblado <86988982+arcticFox-git@users.noreply.github.com>
2023-03-11 21:22:30 -0800

    Add four lanes with colors for states

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./components/hero";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
