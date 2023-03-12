a11b564be7535548b8ca28e9eeab47c7d1a5321c
Mark Pitblado <86988982+arcticFox-git@users.noreply.github.com>
2023-03-11 21:56:32 -0800

    Replaced color palette with images

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import HazardOutrageImage from "../assets/hazard_outrage.jpg";
+import HazardNoOutrageImage from "../assets/hazard_no_outrage.jpg";
+import OutrageNoHazard from "../assets/outrage_no_hazard.jpg";
+import NoOutrageNoHazard from "../assets/no_outrage_no_hazard.jpg";
 
 const Hero = () => (
-  <div class="flex min-h-screen bg-slate-200">
-    <div class="grid grid-cols-1 gap-0 w-full min-h-screen md:grid-cols-2 lg:grid-cols-4">
+  <div class="flex min-h-screen min-w-screen items-center justify-center bg-neutral-800">
+    <div class="grid grid-cols-1 gap-0 md:grid-cols-2 lg:grid-cols-4">
       <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-        <div class="h-96 w-72">
-          <p>Text1</p>
+        <div class="h-full w-full">
+          <img
+            class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+            src={HazardOutrageImage}
+            alt=""
+          />
         </div>
-        <div class="absolute inset-0 bg-[#E56B6F] from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+        <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
         <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
           <h1 class="font-dmserif text-3xl font-bold text-white">Beauty</h1>
           <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
@@ -20,10 +28,14 @@ const Hero = () => (
         </div>
       </div>
       <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-        <div class="h-96 w-72">
-          <p>Text2</p>
+        <div class="h-full w-full">
+          <img
+            class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+            src={HazardNoOutrageImage}
+            alt=""
+          />
         </div>
-        <div class="absolute inset-0 bg-[#B56576] from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+        <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
         <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
           <h1 class="font-dmserif text-3xl font-bold text-white">
             Beyond Builder
@@ -38,8 +50,14 @@ const Hero = () => (
         </div>
       </div>
       <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-        <div class="h-96 w-72"></div>
-        <div class="absolute inset-0 bg-[#6D597A] from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+        <div class="h-full w-full">
+          <img
+            class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+            src={OutrageNoHazard}
+            alt=""
+          />
+        </div>
+        <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
         <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
           <h1 class="font-dmserif text-3xl font-bold text-white">
             Shooting Star
@@ -54,10 +72,18 @@ const Hero = () => (
         </div>
       </div>
       <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-        <div class="h-96 w-72"></div>
-        <div class="absolute inset-0 bg-[#355070] from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+        <div class="h-full w-full">
+          <img
+            class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+            src={NoOutrageNoHazard}
+            alt=""
+          />
+        </div>
+        <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
         <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-          <h1 class="font-dmserif text-3xl font-bold text-white">Beauty</h1>
+          <h1 class="font-dmserif text-3xl font-bold text-white">
+            Shooting Star
+          </h1>
           <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
             dolore adipisci placeat.
