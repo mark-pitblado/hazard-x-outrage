c110fe1c12a1966af7c4638bac8c94c3b18ef2bd
Mark Pitblado <86988982+arcticFox-git@users.noreply.github.com>
2023-03-12 15:38:49 -0700

    Fixing transition speed on first image in hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => (
           />
         </div>
         <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-        <div class="absolute inset-0 flex md:translate-y-[80%] flex-col items-center justify-center px-9 text-center md:transition-all duration-500 group-hover:translate-y-0">
+        <div class="absolute inset-0 flex md:translate-y-[80%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
           <h1 class="font-dmserif text-3xl font-bold text-white">
             Hazard and Outrage
           </h1>
